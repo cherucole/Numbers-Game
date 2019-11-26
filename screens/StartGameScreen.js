e407d31f58b2990cb76e1935bb1bac65beabe095
cherucole <cheruyotcheruyot@gmail.com>
2019-11-26 13:19:39 +0300

    added gamescreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -47,7 +47,12 @@ const StartGameScreen = props => {
       <Card style={styles.summaryContainer}>
         <Text>You Selected:</Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title="START GAME" />
+        <Button
+          title="START GAME"
+          onPress={() => {
+            props.onStartGame(selectedNumber);
+          }}
+        />
       </Card>
     );
   }
