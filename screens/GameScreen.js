651ee1f66ebd778ea36ccc1c8b0943b5c322d075
cherucole <cheruyotcheruyot@gmail.com>
2019-11-30 11:47:42 +0300

    adding flexibility by if and dimensions API

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,7 +5,8 @@ import {
   StyleSheet,
   Alert,
   ScrollView,
-  FlatList
+  FlatList,
+  Dimensions
 } from "react-native";
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -78,6 +79,12 @@ const GameScreen = props => {
       ...curPastGuesses
     ]);
   };
+  let listContainerStyle = styles.listContainer;
+
+  if (Dimensions.get("window").width < 350) {
+    listContainerStyle = styles.listContainerBig;
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.bodyText}>Opponent's Guess:</Text>
@@ -90,7 +97,7 @@ const GameScreen = props => {
           <AntDesign name="caretup" size={24} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
+      <View style={listContainerStyle}>
         {/* <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) =>
             renderListItem(guess, pastGuesses.length - index)
@@ -116,13 +123,17 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%"
   },
   listContainer: {
     flex: 1,
+    // width: Dimensions.get("window").width > 350 ? "60%" : "80%"
     width: "60%"
+  },
+  listContainerBig: {
+    width: "80%"
   },
   list: {
     flexGrow: 1,
